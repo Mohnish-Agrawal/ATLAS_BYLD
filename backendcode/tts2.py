4f95fb5e39d1a708bb1e13b15a4158f1ef88f6d4
Mohnish Agrawal <mohnisha2011@icloud.com>
2020-08-01 00:11:03 +0400

    Updated code for python 3.8

--- backendcode/tts2.py
+++ backendcode/tts2.py
@@ -1,6 +1,10 @@
 from pydub import AudioSegment
 from gtts import gTTS
 import os
+
+# AudioSegment.converter = "C:\\Program Files\\ffmpeg-20200721-b5f1e05-win64-static\\bin\\ffmpeg.exe"
+# AudioSegment.ffmpeg = "C:\\Program Files\\ffmpeg-20200721-b5f1e05-win64-static\\bin\\ffmpeg.exe"
+# AudioSegment.ffprobe = "C:\\Program Files\\ffmpeg-20200721-b5f1e05-win64-static\\bin\\ffprobe.exe"
 def speak(reply):
 	tts=gTTS(reply)
 	tts.save("good.mp3")
@@ -8,4 +12,4 @@ def speak(reply):
 	sound=AudioSegment.from_mp3("good.mp3")
 	sound.export("good.wav", format="wav")
 
-	os.system("aplay -q good.wav")
+	os.system("good.wav")
